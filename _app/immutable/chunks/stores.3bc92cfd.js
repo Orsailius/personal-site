9b42540545f7a548f145081c26303b18e99636ae
Orsailius <undefined>
2023-10-08 10:12:36 -0400

    Updates

--- _app/immutable/chunks/stores.3bc92cfd.js
+++
@@ -1,1 +0,0 @@
-import"./index.65ca13f8.js";import{s as e}from"./singletons.568ee9c9.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};

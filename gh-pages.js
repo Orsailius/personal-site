2ef5c9c771b747ef2da4f4fbbd098d5957eb4dbf
Orsailius <sumemail8472@gmail.com>
2024-07-19 20:02:24 -0400

    pipeline

--- gh-pages.js
+++ gh-pages.js
@@ -5,10 +5,11 @@ publish(
     {
         branch: 'gh-pages',
         git:"F:/Program Files/Git/bin/git.exe",
-        repo: 'https://github.com/Orsailius/personal-site.git', // Update to point to your repository
+        repo: 'https://github.com/Orsailius/personal-site', // Update to point to your repository
         user: 
         {
-            name: 'Orsailius', 
+            name: 'Orsailius',
+            email: 'sumemail8472@gmail.com'
         },
         dotfiles: true,
         nojekyll: true,

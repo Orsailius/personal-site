ffb5445346e9f8e425f82539b92b9fcf9c93dab9
Orsailius <sumemail8472@gmail.com>
2024-08-16 21:56:46 -0400

    Auto Generated Commit

---
+++ _app/immutable/chunks/loop.8031a112.js
@@ -0,0 +1,1 @@
+import{w as i}from"./writeAndUnwriteText.1ae05d4d.js";import{a as n,m as r}from"./animationSetup.aa89d0fb.js";const p=async(t,e)=>{const{options:o,elements:a}=n(t,e);for(;;){r(t);for(const s of a)await i(s,o)}return{update(){},destroy(){}}};export{p as default};
